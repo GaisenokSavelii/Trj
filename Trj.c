67c12ac31f0e4db4d22e70411135d69f2ef798a5
GaisenokSavelii <saveliigaisenok@gmail.com>
2026-03-31 20:54:05 +0300

    refactoring

--- Trj.c
+++ Trj.c
@@ -2,33 +2,33 @@
 #include <stdbool.h>
 #include <Windows.h>
 
-char pattern_yes[] = {"yes"};
-char pattern_no[] = { "yes" };
+char pattern_yes[] = "yes";
+char pattern_no[] = "no";
 
-int Match_Checking(char string[], char pattern[]);
+bool Match_Checking(char string[], char pattern[]);
 
 int main() {
-    printf("You wont't add this app in startup apps? Your ans (yes or no): ");
+    printf("Do you wont add this app in startup apps? Your ans (yes or no): ");
     char yes_or_no[10];
     scanf_s("%9s", yes_or_no, (unsigned)sizeof(yes_or_no));
-    if (Match_Checking(yes_or_no, pattern_yes) >= 0) {
+    if (Match_Checking(yes_or_no, pattern_yes)) {
         printf("Hello world!\n");
     }
 
     return 0;
 }
 
-int Match_Checking(char string[], char pattern[]) {
+bool Match_Checking(char* string, char* pattern) {
     int i, j, k;
     for (i = 0; string[i] != '\0'; i++) {
         for (j = i, k = 0; pattern[j] != '\0' && string[j] == pattern[k]; j++, k++);
 
-        if (k > 0 && pattern[k] == '\0') {
-            return i;
+        if ((k > 0 && pattern[k] == '\0') && (i >= 0)) {            
+             return true;
         }
     }
 
-    return -1;
+    return false;
 }
 
 // path to startup apps: 
