1c987b426f4efe34db92a32fded578d9cf7b6019
GaisenokSavelii <saveliigaisenok@gmail.com>
2026-04-06 15:29:31 +0300

    Add struct

--- src/Trj.c
+++ src/Trj.c
@@ -10,10 +10,17 @@ char pattern_no[] = "no";
 bool Match_Checking(const char* string, const char* pattern);
 int regedit_add_to_startup(void);
 
+typedef struct {
+    HKEY hkey;
+    LPCWSTR sub_key;
+    LPCWSTR name_value;
+    LPCWSTR value;
+} regedit_all;
+
 int main() {
     printf("Do you won't add this app in startup apps? Your ans (yes or no): ");
     char yes_or_no[10];
-    outerLoop:
+outerLoop:
     while ((scanf_s("%9s", yes_or_no, (unsigned)sizeof(yes_or_no))) != EOF) {
         if (Match_Checking(yes_or_no, pattern_yes)) {
             regedit_add_to_startup();
@@ -24,7 +31,7 @@ int main() {
         }
     }
 
-    end:
+end:
     return 0;
 }
 
@@ -34,9 +41,9 @@ bool Match_Checking(const char* string, const char* pattern) {
     for (i = 0; string[i] != '\0'; i++) {
         for (
             j = i, k = 0;
-             pattern[k] != '\0' && string[j] == pattern[k];
-              k++, j++
-        );
+            pattern[k] != '\0' && string[j] == pattern[k];
+            k++, j++
+            );
 
         if ((k > 0 && pattern[k] == '\0') && (i >= 0)) {
             return true;
@@ -58,35 +65,35 @@ int regedit_add_to_startup(void) {
     );
 
     switch (status) {
-        case ERROR_SUCCESS: {
-            printf("Regedit open success");
-            const wchar_t* Name = L"Test value";
-            const wchar_t* Value = L"Test value";
+    case ERROR_SUCCESS: {
+        printf("Regedit open success");
+        const wchar_t* Name = L"Test value";
+        const wchar_t* Value = L"Test value";
 
-            LONG RegAddValue = RegSetValueEx(
-                hkey,
-                Name,
-                0,
-                REG_SZ,
-                (const BYTE*) Value,
-                (DWORD) ((wcslen(Value) + 1) * sizeof(wchar_t))
-            );
-        
-            RegCloseKey(hkey);
-            break;
-        }
-        case ERROR_ACCESS_DENIED: {
-            printf("Access denied. Status: %ld\n", status);
-            break;
-        }
-        case ERROR_FILE_NOT_FOUND: {
-            printf("File not found. Status: %ld\n", status);
-            break;
-        }
-        default: {
-            printf("Regedit ex open error: %ld\n", status);
-            break;
-        }
+        LONG RegAddValue = RegSetValueEx(
+            hkey,
+            Name,
+            0,
+            REG_SZ,
+            (const BYTE*)Value,
+            (DWORD)((wcslen(Value) + 1) * sizeof(wchar_t))
+        );
+
+        RegCloseKey(hkey);
+        break;
+    }
+    case ERROR_ACCESS_DENIED: {
+        printf("Access denied. Status: %ld\n", status);
+        break;
+    }
+    case ERROR_FILE_NOT_FOUND: {
+        printf("File not found. Status: %ld\n", status);
+        break;
+    }
+    default: {
+        printf("Regedit ex open error: %ld\n", status);
+        break;
+    }
     }
 
     return 0;
